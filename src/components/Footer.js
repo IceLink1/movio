edb0a7871340e9eb5adc2885ea73dff57d3daa31
IceLink1 <163158076+IceLink1@users.noreply.github.com>
2024-08-17 19:06:38 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
           <Link to="/" >About</Link>
           <Link to="/">Contact</Link>
         </div>
-        <p className='text-sm'>Created By Dynamic Coding with Amit</p>
+        <p className='text-sm'>Created By ICELINK </p>
     </footer>
   )
 }
